50fabb7f36a292a94b008e37c5b9133c9c2a3f71
Angelvilad <angelvilad@hotmail.com>
2018-12-17 00:09:26 +0100

    Modificado fecha de expiracion de cookie con las preferencias de idioma

--- routes/lang.js
+++ routes/lang.js
@@ -11,7 +11,7 @@ router.get('/:locale', (req, res, next) => {
     const backTo = req.get('referer');
 
     // establecemos la cookie del nuevo idioma
-    res.cookie('nodepop-lang', locale, { maxAge: 1000 * 60 * 60 * 24 }); //en opciones, caducidad 1 dia
+    res.cookie('nodepop-lang', locale, { maxAge: 1000 * 60 * 60 * 24 * 2 }); //en opciones, caducidad 2 dias
 
     // redirigimos al usuario donde estaba
     res.redirect(backTo);
